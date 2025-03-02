be2d1145bc2560e04130974da18a19e3eee7e0fc
Master-mohit <mohitmansigupta9098@gmail.com>
2025-03-02 20:29:42 +0530

    execution context

--- name.js
+++ name.js
@@ -123,3 +123,110 @@
 // console.log(a);
 // }
 // console.log(b);
+
+
+// .........................Closure.....................................❕❕❕
+// function outer(){
+//     let a = 10;
+//     function inner(){
+//         let b = 20;
+//         console.log(a + b);
+//     }
+//     return inner;
+// }
+// const totl = outer();
+// totl();
+
+
+// function restaurant(){
+//     let order = "pizza";
+
+//     function kichen(){
+//         console.log(order);
+//     }
+//     return kichen;
+// }
+// const getorder = restaurant();
+// getorder();
+
+// .........................Promise.....................................❕❕❕
+// function fetchdata(){
+//    return new Promise((res, rej) => {
+//         let success = true;
+//         if(success){
+//             res("data fetched");
+//         }
+//         else {
+//             rej("data not fetched");
+//         }
+//     })
+// }
+// fetchdata()
+//           .then((result) => console.log(result))
+//           .catch((error) => console.log(error))
+//           .finally(() => console.log("done"));
+
+ 
+
+// .............................async and await..............................❕❕❕
+// function orderFood(){
+//     return new Promise((res)=> {
+//         setTimeout(()=> {
+//             res("food is ready");
+//         }, 2000)
+//     })
+// }
+
+// async function eat(){
+//     let food = await orderFood();
+//     console.log(food);
+// }
+// eat();
+
+
+// function order(isAvailable){
+//     return new Promise((res, rej)=> {
+//         setTimeout(() => {
+//             if(isAvailable){
+//                 res("food is ready");
+//             }
+//             else {
+//                 rej("food is not ready");
+//             }
+//         }, 2000)
+//     })
+// }
+// async function eat(){
+//     let food = await order(true);
+//     console.log(food);
+// }
+// eat();
+
+// .............................Execution context..............................❕❕❕
+// function greet(){
+//     console.log(" hello there mohit");
+// }
+// greet();
+
+// ...........................Call stack in JS.........................
+// function first(){
+//     console.log("first function");
+//     second();
+// }
+// function second(){
+//     console.log("second function");
+//     third();
+// }
+// function third(){
+//     console.log("third function");
+// }
+
+// first();
+
+// ...............................Web API.............................
+// console.log("Start");
+// setTimeout(() => {
+//     console.log("2 sec timer");
+// }, 2000);
+// console.log("End");
+
